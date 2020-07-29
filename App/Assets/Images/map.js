b0ebd76cab218b32be258cbc42dbd1fb088df792
yonotest333 <yonotest333@gmail.com>
2020-07-29 09:17:48 +0530

    image removed

--- App/Assets/Images/map.js
+++ App/Assets/Images/map.js
@@ -1,7 +1,4 @@
 /* eslint-disable global-require */
 export const login = {
-  appIcon: require('./ic_app_icon.png'),
-  appPassword: require('./ic_password.png'),
-  appUser: require('./ic_user.png'),
-  appWorkshop: require('./ic_workshp_url.png'),
+
 };
